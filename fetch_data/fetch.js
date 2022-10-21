708225b8a3309c618f7bdcf638a3bfce3afa7311
Se Hyun Heo <112599940+shheo21@users.noreply.github.com>
2022-10-21 16:55:33 +0900

    [Add] package.json, fetch.js(ott 서비스 목록 크롤링)

---
+++ fetch_data/fetch.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+axios.get(`https://api.themoviedb.org/3/watch/providers/movie?api_key=6c616e9a21c6e51d1191f6fc4f2d11c4&language=ko-KR`)
+    .then(pvlist => {
+        console.log(pvlist.data);
+    });
