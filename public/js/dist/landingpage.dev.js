3bbf43782b956b75d8d58685eae51f26eb570fe3
Se Hyun Heo <112599940+shheo21@users.noreply.github.com>
2022-11-25 15:50:05 +0900

    [Mod] 영화 및 시리즈 인기 리스트 가져오는 위치 변경

--- public/js/dist/landingpage.dev.js
+++ public/js/dist/landingpage.dev.js
@@ -2,22 +2,26 @@
 
 var _signIn = require("../js/signIn.js");
 
-var api_key = '6c616e9a21c6e51d1191f6fc4f2d11c4';
-var $search_form = document.querySelector('.search_form');
-var $search_txt = document.querySelector('.search_txt');
-var $result_contents = document.querySelector('.result_contents');
-var $seriesList = document.querySelector('.seriesList');
+var api_key = '6c616e9a21c6e51d1191f6fc4f2d11c4'; //const $search_form = document.querySelector('.search_form');
+//const $search_txt = document.querySelector('.search_txt');s
+
+var $result_contents = document.querySelector('.result_contents'); //const $seriesList = document.querySelector('.seriesList');
+
 var $movieList = document.querySelector('.movieList');
 var $fragment = document.createDocumentFragment();
-var signInBtn = document.querySelector('#signInBtn');
-console.log(_signIn.email);
+var signInBtn = document.querySelector('#signInBtn'); //console.log(email);
 
-$search_form.onsubmit = function (e) {
-  e.preventDefault();
-  $result_contents.innerHTML = '';
-  console.log($search_form.querySelector('input').value);
-  render();
-};
+var $seriesTopList = document.querySelector('.top10-serieslist'); // TOP 10 영화 리스트 접근 변수
+
+var $moviesTopList = document.querySelector('.top10-movieslist'); // TOP 10 시리즈 리스트 접근 변수
+//console.log(email);
+
+/*$search_form.onsubmit = e => {
+    e.preventDefault();
+    $result_contents.innerHTML = '';
+    console.log($search_form.querySelector('input').value);
+};*/
+// 영화 목록 가져오기 함수
 
 var getMovieList = function getMovieList() {
   var getMovie, _ref, results;
@@ -28,7 +32,7 @@ var getMovieList = function getMovieList() {
         case 0:
           _context.prev = 0;
           _context.next = 3;
-          return regeneratorRuntime.awrap(fetch("https://api.themoviedb.org/3/movie/popular?api_key=".concat(api_key, "&language=ko-KR&page=1")));
+          return regeneratorRuntime.awrap(fetch("https://api.themoviedb.org/3/movie/popular?api_key=".concat(api_key, "&language=ko-KR&page=1&region=KR")));
 
         case 3:
           getMovie = _context.sent;
@@ -39,12 +43,14 @@ var getMovieList = function getMovieList() {
           _ref = _context.sent;
           results = _ref.results;
           results.forEach(function (content) {
-            var $li_series = document.createElement('li_series');
-            $li_series.id = content.id;
-            $li_series.className = content.media_type;
-            var $a = document.createElement('a');
+            var $li_movies = document.createElement('li'); // ul 안의 각각의 리스트 요소
+
+            $li_movies.id = content.id; // 사진의 id를 id에 저장
+
+            var $a = document.createElement('a'); // 링크 태그 생성
+
             $a.href = '#';
-            var $img = document.createElement('img');
+            var $img = document.createElement('img'); // 링크 태그 내에 넣을 이미지 생성
 
             if (content.poster_path == null) {
               $img.src = '../image/notready.png';
@@ -52,12 +58,13 @@ var getMovieList = function getMovieList() {
               $img.src = "https://image.tmdb.org/t/p/w500/".concat(content.poster_path);
             }
 
-            var textNode = document.createTextNode(content.title);
+            var textNode = document.createTextNode(content.title); // 영화 제목 저장
+
             $a.append($img);
             $a.append(textNode);
-            $li_series.append($a);
-            $fragment.append($li_series);
-            $result_contents.append($fragment);
+            $li_movies.append($a);
+            $fragment.append($li_movies);
+            $moviesTopList.append($fragment);
           });
           _context.next = 14;
           break;
@@ -75,7 +82,7 @@ var getMovieList = function getMovieList() {
   }, null, null, [[0, 11]]);
 };
 
-getMovieList();
+getMovieList(); // 시리즈 리스트 가져오는 함수
 
 var getSeriesList = function getSeriesList() {
   var getSeries, _ref2, results;
@@ -86,7 +93,7 @@ var getSeriesList = function getSeriesList() {
         case 0:
           _context2.prev = 0;
           _context2.next = 3;
-          return regeneratorRuntime.awrap(fetch("https://api.themoviedb.org/3/tv/popular?api_key=".concat(api_key, "&language=ko-KR&page=1")));
+          return regeneratorRuntime.awrap(fetch("https://api.themoviedb.org/3/tv/popular?api_key=".concat(api_key, "&language=ko-KR&page=1&region=KR")));
 
         case 3:
           getSeries = _context2.sent;
@@ -97,12 +104,13 @@ var getSeriesList = function getSeriesList() {
           _ref2 = _context2.sent;
           results = _ref2.results;
           results.forEach(function (content) {
-            var $li = document.createElement('li');
-            $li.id = content.id;
-            $li.className = content.media_type;
-            var $a = document.createElement('a');
+            var $li_series = document.createElement('li'); // 각각의 리스트 요소
+
+            $li_series.id = content.id;
+            var $a = document.createElement('a'); // 링크 생성
+
             $a.href = '#';
-            var $img = document.createElement('img');
+            var $img = document.createElement('img'); // 링크 내에 들어갈 이미지 생성
 
             if (content.poster_path == null) {
               $img.src = '../image/notready.png';
@@ -110,12 +118,16 @@ var getSeriesList = function getSeriesList() {
               $img.src = "https://image.tmdb.org/t/p/w500/".concat(content.poster_path);
             }
 
-            var textNode = document.createTextNode(content.title);
-            $a.append($img);
-            $a.append(textNode);
-            $li.append($a);
-            $fragment.append($li);
-            $result_contents.append($fragment);
+            var textNode = document.createTextNode(content.name);
+            $a.append($img); // 링크에 이미지 추가
+
+            $a.append(textNode); // 링크에 영화 제목 추가
+
+            $li_series.append($a); // 리스트에 해당 링크(이미지 + 제목) 추가
+
+            $fragment.append($li_series); // 해당 리스트 묶음을 전달 개체에 추가
+
+            $seriesTopList.append($fragment); // html에 전달 개체의 내용 추가
           });
           _context2.next = 14;
           break;
@@ -134,66 +146,3 @@ var getSeriesList = function getSeriesList() {
 };
 
 getSeriesList();
-
-var render = function render() {
-  var resContent, _ref3, results, movieString;
-
-  return regeneratorRuntime.async(function render$(_context3) {
-    while (1) {
-      switch (_context3.prev = _context3.next) {
-        case 0:
-          _context3.prev = 0;
-          _context3.next = 3;
-          return regeneratorRuntime.awrap(fetch("https://api.themoviedb.org/3/search/multi?api_key=".concat(api_key, "&language=ko-KR&query=").concat($search_txt.value, "&page=1&include_adult=false&region=KR")));
-
-        case 3:
-          resContent = _context3.sent;
-          _context3.next = 6;
-          return regeneratorRuntime.awrap(resContent.json());
-
-        case 6:
-          _ref3 = _context3.sent;
-          results = _ref3.results;
-
-          _signIn.movies.push($search_txt.value);
-
-          movieString = JSON.stringify(_signIn.movies);
-          localStorage.setItem(_signIn.movies, movieString); //localStorage.getItem(obj_user);
-          //console.log(obj_user);
-
-          results.forEach(function (content) {
-            var $li = document.createElement('li');
-            $li.id = content.id;
-            $li.className = content.media_type;
-            var $a = document.createElement('a');
-            $a.href = '#';
-            var $img = document.createElement('img');
-
-            if (content.poster_path == null) {
-              $img.src = '../image/notready.png';
-            } else {
-              $img.src = "https://image.tmdb.org/t/p/w500/".concat(content.poster_path);
-            }
-
-            var textNode = document.createTextNode(content.title);
-            $a.append($img);
-            $a.append(textNode);
-            $li.append($a);
-            $fragment.append($li);
-            $result_contents.append($fragment);
-          });
-          _context3.next = 17;
-          break;
-
-        case 14:
-          _context3.prev = 14;
-          _context3.t0 = _context3["catch"](0);
-          console.log('[ERROR]' + _context3.t0);
-
-        case 17:
-        case "end":
-          return _context3.stop();
-      }
-    }
-  }, null, null, [[0, 14]]);
-};
--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -4,83 +4,87 @@ import {
     movies
 } from "../js/signIn.js";
 const api_key = '6c616e9a21c6e51d1191f6fc4f2d11c4';
-const $search_form = document.querySelector('.search_form');
-const $search_txt = document.querySelector('.search_txt');
+//const $search_form = document.querySelector('.search_form');
+//const $search_txt = document.querySelector('.search_txt');s
 const $result_contents = document.querySelector('.result_contents');
-const $seriesList = document.querySelector('.seriesList');
+//const $seriesList = document.querySelector('.seriesList');
 const $movieList = document.querySelector('.movieList');
 const $fragment = document.createDocumentFragment();
 const signInBtn = document.querySelector('#signInBtn');
 //console.log(email);
-$search_form.onsubmit = e => {
+const $seriesTopList = document.querySelector('.top10-serieslist'); // TOP 10 영화 리스트 접근 변수
+const $moviesTopList = document.querySelector('.top10-movieslist'); // TOP 10 시리즈 리스트 접근 변수
+//console.log(email);
+/*$search_form.onsubmit = e => {
     e.preventDefault();
     $result_contents.innerHTML = '';
     console.log($search_form.querySelector('input').value);
-    render();
-};
+};*/
+
+// 영화 목록 가져오기 함수
 const getMovieList = async () => {
     try {
         const getMovie = await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=ko-KR&page=1`
+            `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=ko-KR&page=1&region=KR`
         );
         const {
             results
         } = await getMovie.json();
         results.forEach(content => {
-            const $li_series = document.createElement('li_series');
-            $li_series.id = content.id;
-            $li_series.className = content.media_type;
-            const $a = document.createElement('a');
+            const $li_movies = document.createElement('li'); // ul 안의 각각의 리스트 요소
+            $li_movies.id = content.id; // 사진의 id를 id에 저장
+            const $a = document.createElement('a'); // 링크 태그 생성
             $a.href = '#';
-            const $img = document.createElement('img');
+            const $img = document.createElement('img'); // 링크 태그 내에 넣을 이미지 생성
             if (content.poster_path == null) {
                 $img.src = '../image/notready.png';
             } else {
                 $img.src = `https://image.tmdb.org/t/p/w500/${content.poster_path}`;
             }
-            const textNode = document.createTextNode(content.title);
+            const textNode = document.createTextNode(content.title); // 영화 제목 저장
 
             $a.append($img);
             $a.append(textNode);
-            $li_series.append($a);
-            $fragment.append($li_series);
-            $result_contents.append($fragment);
+            $li_movies.append($a);
+            $fragment.append($li_movies);
+            $moviesTopList.append($fragment);
         });
     } catch (err) {
         console.log('[ERROR]' + err);
     }
 }
-//getMovieList();
+getMovieList();
+
+// 시리즈 리스트 가져오는 함수
 const getSeriesList = async () => {
     try {
         const getSeries = await fetch(
-            `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=ko-KR&page=1`
+            `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=ko-KR&page=1&region=KR`
         );
         const {
             results
         } = await getSeries.json();
         results.forEach(content => {
-            const $li = document.createElement('li');
-            $li.id = content.id;
-            $li.className = content.media_type;
-            const $a = document.createElement('a');
+            const $li_series = document.createElement('li'); // 각각의 리스트 요소
+            $li_series.id = content.id;
+            const $a = document.createElement('a'); // 링크 생성
             $a.href = '#';
-            const $img = document.createElement('img');
+            const $img = document.createElement('img'); // 링크 내에 들어갈 이미지 생성
             if (content.poster_path == null) {
                 $img.src = '../image/notready.png';
             } else {
                 $img.src = `https://image.tmdb.org/t/p/w500/${content.poster_path}`;
             }
-            const textNode = document.createTextNode(content.title);
+            const textNode = document.createTextNode(content.name);
 
-            $a.append($img);
-            $a.append(textNode);
-            $li.append($a);
-            $fragment.append($li);
-            $result_contents.append($fragment);
+            $a.append($img); // 링크에 이미지 추가
+            $a.append(textNode); // 링크에 영화 제목 추가
+            $li_series.append($a); // 리스트에 해당 링크(이미지 + 제목) 추가
+            $fragment.append($li_series); // 해당 리스트 묶음을 전달 개체에 추가
+            $seriesTopList.append($fragment); // html에 전달 개체의 내용 추가
         });
     } catch (err) {
         console.log('[ERROR]' + err);
     }
 }
-//getSeriesList();
+getSeriesList();
