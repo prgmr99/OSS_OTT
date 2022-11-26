56ce7663fa7f32de42d8428aea03c481e705bc70
prgmr99 <johnyeom24@naver.com>
2022-11-26 21:33:37 +0900

    [Etc] 파일 정리

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,5 @@
+//import { obj_user } from "./signIn";
+
 const api_key = '6c616e9a21c6e51d1191f6fc4f2d11c4';
 const $search_form = document.querySelector('.search_form');
 const $search_txt = document.querySelector('.search_txt');
@@ -5,20 +7,35 @@ const $result_contents = document.querySelector('.result_contents');
 const $seriesList = document.querySelector('.seriesList');
 const $movieList = document.querySelector('.movieList');
 const $fragment = document.createDocumentFragment();
+const signInBtn = document.querySelector('#signInBtn');
+//console.log(email);
+let obj = JSON.parse(localStorage.getItem('obj_user'));
+console.log(obj);
+//console.log(obj.user);
 
 $search_form.onsubmit = e => {
     e.preventDefault();
     $result_contents.innerHTML = '';
-    console.log($search_form.querySelector('input').value);
+    //console.log($search_form.querySelector('input').value);
     render();
 };
-a
+
 const render = async () => {
     try {
-        const resContent = await fetch(          
+        const resContent = await fetch(
             `https://api.themoviedb.org/3/search/multi?api_key=${api_key}&language=ko-KR&query=${$search_txt.value}&page=1&include_adult=false&region=KR`
         );
-        const { results } = await resContent.json();
+        const {
+            results
+        } = await resContent.json();
+        //console.log($search_txt.value);
+
+        const bmBtn = document.querySelector("#bookMarkBtn");
+        bmBtn.addEventListener('click', () => {
+            let mvList = JSON.parse(localStorage.getItem('obj_user'));
+            (mvList.mv).push($search_txt.value);
+            localStorage.setItem('obj_user', JSON.stringify(mvList));
+        })
         results.forEach(content => {
             const $li = document.createElement('li');
             $li.id = content.id;
@@ -27,11 +44,17 @@ const render = async () => {
             $a.href = '#';
             const $img = document.createElement('img');
             if (content.poster_path == null) {
-                $img.src =  '../image/notready.png';
+                $img.src = '../image/notready.png';
             } else {
-                $img.src = `https://image.tmdb.org/t/p/w500/${content.poster_path}`;
+                $img.src = `https://image.tmdb.org/t/p/w200/${content.poster_path}`;
             }
-            const textNode = document.createTextNode(content.title);
+            var textNode;
+            if (content.media_type === 'movie') {
+                textNode = document.createTextNode(content.title);
+            }
+            else if (content.media_type === 'tv') {
+                textNode = document.createTextNode(content.name);
+            }
             $a.append($img);
             $a.append(textNode);
             $li.append($a);
---
+++ src/js/signIn.js
@@ -0,0 +1,99 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDpPAcdVQmUoLwiXTEO_6PRuzloKDjYX3A",
+    authDomain: "auth-test-web-1-4ae93.firebaseapp.com",
+    databaseURL: "https://auth-test-web-1-4ae93-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "auth-test-web-1-4ae93",
+    storageBucket: "auth-test-web-1-4ae93.appspot.com",
+    messagingSenderId: "977888738182",
+    appId: "1:977888738182:web:98e6f6f6a04f0089914a07",
+    measurementId: "G-6NFEXG2TZW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+export let email;
+export let movies;
+export let obj_user;
+
+function goUrl() {
+    let link = '../html/LandingPage.html';
+    location.href = link;
+}
+
+const test1 = function() {
+        email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        movies = [];
+
+        obj_user = {
+            user: email,
+            mv: movies
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            const dt = new Date();
+            update(ref(database, 'users/' + user.uid), {
+                last_login: dt
+            })
+            alert('User Sign In!');
+            // localStorage에 user : email 값 추가.
+            localStorage.setItem('obj_user', JSON.stringify(obj_user));
+            signInbtn.removeEventListener("click", test1);
+            goUrl();
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            alert(errorMessage);
+        })
+    }
+
+let signInbtn = document.getElementById('signInBtn')
+console.log(signInbtn);
+signInbtn.addEventListener('click', test1, {once: true});
+
+
+
+
+
+
+/*
+const user = auth.currentUser;
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+    } else {
+        // 유저 로그아웃 구현 가능.
+    }
+});*/
+
+// 로그아웃 -> 나중에 아래의 signOut함수만 호출하면 로그아웃이 된다.
+/**logout.addEventListener('click', (e) => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        alert('User Sign Out!');
+        }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        
+        alert(errorMessage);
+    })
+});*/
